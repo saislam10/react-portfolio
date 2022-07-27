c8852d12e313080f54c187d1aaff4669aa51ad74
Safwan Islam <saislam@davidson.edu>
2022-07-27 11:21:06 -0500

    added logo links, all ready to turn in, will check heroku deployment

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,9 @@ import '../styles/Footer.css';
 function Footer() {
   return (
     <footer className="footer">
-      
-          <a id="LI" href="https://www.linkedin.com/in/safwan-islam-70a095246//"> </a>
-          <a id="GH" href="https://github.com/saislam10"> </a>
-
+          <a href="https://github.com/saislam10"> <img src="https://img.icons8.com/fluent/48/000000/github.png" alt="GitHub" className="Icon"/></a>
+          <a href="https://www.linkedin.com/in/safwan-islam-70a095246//"> <img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn" className="Icon"/></a>
+          <a href="https://stackoverflow.com/users/18367681/safwanislam10"> <img src="https://cdn-icons-png.flaticon.com/512/2111/2111640.png" alt="StackOverFlow" className="Icon"/></a>
     </footer>
   );
 }
